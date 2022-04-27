6f9eca584e9794b62bb84a4a4e292465713cdfd3
ihm-tswow <ihm-ts@protonmail.com>
2022-04-27 15:16:07 +0200

    Core/Objects: Remove SetInt16Value

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -115,7 +115,6 @@ class TC_GAME_API Object
         void SetFloatValue(uint16 index, float value);
         void SetByteValue(uint16 index, uint8 offset, uint8 value);
         void SetUInt16Value(uint16 index, uint8 offset, uint16 value);
-        void SetInt16Value(uint16 index, uint8 offset, int16 value) { SetUInt16Value(index, offset, (uint16)value); }
         void SetGuidValue(uint16 index, ObjectGuid value);
         void SetStatFloatValue(uint16 index, float value);
         void SetStatInt32Value(uint16 index, int32 value);
